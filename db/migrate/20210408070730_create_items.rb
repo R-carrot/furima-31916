d1dfb7619d4367e60980856cef4442da49982183
R-carrot <r.carrot.9@gmail.com>
2021-04-09 13:10:04 +0900

    DB作成

---
+++ db/migrate/20210408070730_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+
+      t.timestamps
+      t.string     :title,           null: false
+      t.text       :explain,         null: false
+      t.integer    :category_id,     null: false
+      t.integer    :state_id,        null: false
+      t.integer    :delivery_fee_id, null: false
+      t.integer    :pregecture_id,   null: false
+      t.integer    :delivery_fee_id, null: false
+      t.integer    :price,           null: false
+      t.references :user,            foreign_key: true
+    end
+  end
+end
