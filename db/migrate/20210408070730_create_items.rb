bd17eab826872bce8670d3615d1f0c5df474faa9
R-carrot <r.carrot.9@gmail.com>
2021-04-09 13:45:58 +0900

    配布データの追加

--- db/migrate/20210408070730_create_items.rb
+++ db/migrate/20210408070730_create_items.rb
@@ -9,7 +9,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer    :state_id,        null: false
       t.integer    :delivery_fee_id, null: false
       t.integer    :pregecture_id,   null: false
-      t.integer    :delivery_fee_id, null: false
+      t.integer    :delivery_date_id, null: false
       t.integer    :price,           null: false
       t.references :user,            foreign_key: true
     end
