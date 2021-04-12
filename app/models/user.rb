87b223e039b817ab040ffb9319662085af841c0c
R-carrot <r.carrot.9@gmail.com>
2021-04-13 06:50:39 +0900

    userモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,13 @@ class User < ApplicationRecord
   has_many :buyers
 
   validates :nickname,             presence: true
-  validates :first_name,           presence: true
-  validates :last_name,            presence: true
-  validates :first_name_katakana,  presence: true
-  validates :last_name_katakana,   presence: true
+  validates :first_name,           presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
+  validates :first_name_katakana,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_katakana,   presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_day,            presence: true
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :encrypted_password, format: { with: VALID_PASSWORD_REGEX }
 end
