dfd5ead8eca7c37c3fa9df6a720260fa662ea4de
R-carrot <r.carrot.9@gmail.com>
2021-04-15 02:57:05 +0900

    価格のバリデーション設定
    半角数字のみ入力可能
    300円~9,999,999円の価格のみ入力可能

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :title
     validates :explain
-    validates :price
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { in: 300..9999999}
   end
 
   with_options numericality: { other_than: 1 } do
