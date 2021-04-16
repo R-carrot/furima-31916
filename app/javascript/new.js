26bd498600e19b06ca52acd0d3cdc4ffb76b7ff6
R-carrot <r.carrot.9@gmail.com>
2021-04-16 11:23:04 +0900

    指摘された箇所の修正
    デバッグ用コードの削除

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -2,12 +2,11 @@ window.addEventListener('load', function(){
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", function(){
     const inputValue = priceInput.value;
-    console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price");
     const profit = document.getElementById("profit");
     if ( 300 <= inputValue && inputValue <= 9999999 ) {
       addTaxDom.innerHTML = Math.floor( inputValue / 10);
-      const value = inputValue / 10
+      const value = inputValue / 10;
       profit.innerHTML = Math.floor( inputValue - value );
       console.log(addTaxDom);
       console.log(profit);
