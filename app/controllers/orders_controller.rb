baca35ee2fa860f253ac35c7ced4515d101ebe04
R-carrot <r.carrot.9@gmail.com>
2021-04-30 11:35:16 +0900

    クレジットカード機能の導入
    Payjpの導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,38 @@
 class OrdersController < ApplicationController
   def index
-    # @order = Order.new(order_params)
+    @order = BuyerOrder.new
+    # set_item
+  end
+
+  def create
+    @order = BuyerOrder.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render :index
+    end
   end
 
 
-  # private
+  private
 
-  # def order_params
-  #   params.permit(:postail_code, :pregecture_id, :city, :address, :building, :phone_number).merge(buyers: current_user.id)
-  # end
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def order_params
+    params.require(:buyer_order).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id).merge(token: params[:token], user_id: current_user.id)
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+  end
 end
