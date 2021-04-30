765df94cf1417532c9dbec0b2bbb0afca991c72d
R-carrot <r.carrot.9@gmail.com>
2021-04-30 11:33:48 +0900

    formオブジェクトの作成

---
+++ app/models/buyer_order.rb
@@ -0,0 +1,19 @@
+class BuyerOrder
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id, :buyers_id, :token
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/}
+    validates :token
+  end
+
+
+  def save
+    buyer = Buyer.create(user_id: user_id, item_id: item_id)
+    Order.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, buyers_id: buyer.id)
+  end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :buyer
 
-  include ActiveModel::Model
-  attr_accessor 
+  # include ActiveModel::Model
+  # attr_accessor :token
+
+  # validates :token, presence: true
 end
